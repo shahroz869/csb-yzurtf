d4287cac2cfb3be6d9c7854c3edf0ced19c20650
Code Sandbox <hello@codesandbox.io>
2023-05-02 05:47:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.34e5bbf7d2dc360d92077249c2001e21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad957b0323683d2113a53167281fe9f6",
+    "url": "https://.github.io/csb-yzurtf/index.html"
+  },
+  {
+    "revision": "855a89136394b8916cbc",
+    "url": "https://.github.io/csb-yzurtf/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "3ad784398ec6227b399a",
+    "url": "https://.github.io/csb-yzurtf/static/js/2.93b78f3f.chunk.js"
+  },
+  {
+    "revision": "855a89136394b8916cbc",
+    "url": "https://.github.io/csb-yzurtf/static/js/main.8d33b317.chunk.js"
+  },
+  {
+    "revision": "c8dd7388d884c3d6bfe5",
+    "url": "https://.github.io/csb-yzurtf/static/js/runtime~main.1e610cba.js"
+  }
+]);
